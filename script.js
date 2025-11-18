d73de5454119147c8cca43f675559e7d9dd1a4a9
Kyle Hines <disciplezero7@gmail.com>
2025-11-18 14:29:56 -0600

    decimal and backspace function

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-const display = document.querySelector("#display");
-
-let currentNumber = "0";
-let lastNumber = "";
-let operatorSign = null;
-
 const numberBtns = document.querySelectorAll(".number-button");
+const decimalBtn = document.querySelector(".decimal-button");
 const operatorBtns = document.querySelectorAll(".operator-button");
 const equalBtn = document.querySelector(".equal-button");
 const clearBtn = document.querySelector(".clear-button");
 const operatorSelected = document.querySelector(".operator-selected");
+const backBtn = document.querySelector(".back-button");
 
+let currentNumber = "0";
+let lastNumber = "";
+let operatorSign = null;
 let result = false;
 
 // Number buttons
@@ -27,9 +26,22 @@ numberBtns.forEach((e) => {
             currentNumber += e.textContent;
         }
 
-        display.textContent = currentNumber;
+        updateDisplay(currentNumber);
     })
 });
+
+// Decimal button
+decimalBtn.addEventListener("click", () => {
+    if (result === true) {
+        currentNumber = "";
+        result = false;
+    }
+
+    if (!currentNumber.includes(".") && result === false) {
+        currentNumber += ".";
+        updateDisplay(currentNumber);
+    }
+})
 
 // Operator buttons
 operatorBtns.forEach((e) => {
@@ -43,9 +55,8 @@ operatorBtns.forEach((e) => {
             operatorSelected.textContent = operatorSign;
             return;
         } else {
-            display.textContent = operation(lastNumber, currentNumber, operatorSign);
-            
             lastNumber = operation(lastNumber, currentNumber, operatorSign);
+            updateDisplay(lastNumber);
             currentNumber = "";
         }
 
@@ -59,8 +70,8 @@ equalBtn.addEventListener("click", () => {
     // Only runs if operator has been selected
     if (operatorSign !== "") {
         result = true;
-        display.textContent = operation(lastNumber, currentNumber, operatorSign);
-        currentNumber = operation(lastNumber, currentNumber, operatorSign);
+        currentNumber = String(operation(lastNumber, currentNumber, operatorSign));
+        updateDisplay(currentNumber);
         lastNumber = "";
         operatorSign = "";
         operatorSelected.textContent = "";
@@ -73,42 +84,37 @@ clearBtn.addEventListener("click", () => {
     lastNumber = "";
     operatorSign = "";
     operatorSelected.textContent = "";
-    display.textContent = currentNumber;
+    updateDisplay(currentNumber);
 })
 
-
-function add(num1, num2) {
-    return Number(num1) + Number(num2);
-}
-
-function subtract(num1, num2) {
-    return num1 - num2;
-}
-
-function mulitply(num1, num2) {
-    return num1 * num2;
-}
-
-function divide(num1, num2) {
-    if (num2 === "0") {
-        alert("Very funny...");
-        return 0;
-    }
-    return num1 / num2;
-}
+// Back button
+backBtn.addEventListener("click", () => {
+    currentNumber = currentNumber.slice(0, -1);
+    updateDisplay(currentNumber);
+})
 
 function operation(num1, num2, operator) {
+
     switch (operator) {
         case "+":
-            return add(num1, num2);
+            return Number(num1) + Number(num2);
 
         case "-":
-            return subtract(num1, num2);
+            return Number(num1) - Number(num2);
 
         case "*":
-            return mulitply(num1, num2);
+            return Number(num1) * Number(num2);
 
         case "/":
-            return divide(num1, num2);
+            if (Number(num2) === 0) {
+                alert("Very funny...");
+                return 0;
+            }       
+            return Number(num1) / Number(num2);
     }   
+}
+
+function updateDisplay(number) {
+    const display = document.querySelector("#display");
+    display.textContent = number;
 }
